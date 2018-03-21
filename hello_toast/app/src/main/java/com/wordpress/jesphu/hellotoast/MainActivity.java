a76f568ec1f33d36d2f64a27b9b8557518a2b93d
Jessica Phuong <jessicaphuong02@gmail.com>
2018-03-21 16:29:35 +1030

    Add counting functionality to count button

--- hello_toast/app/src/main/java/com/wordpress/jesphu/hellotoast/MainActivity.java
+++ hello_toast/app/src/main/java/com/wordpress/jesphu/hellotoast/MainActivity.java
@@ -4,14 +4,19 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mShowCount = (TextView) findViewById(R.id.show_count); // Get reference to the count.
     }
 
     public void showToast(View view) {
@@ -21,5 +26,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 }
