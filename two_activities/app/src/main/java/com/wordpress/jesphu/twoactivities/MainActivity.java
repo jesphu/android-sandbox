ec3222c06169307e874a0d7da7528baa48991cd4
Jessica Phuong <jessicaphuong02@gmail.com>
2018-03-31 15:45:57 +1030

    Add button and check button click response

--- two_activities/app/src/main/java/com/wordpress/jesphu/twoactivities/MainActivity.java
+++ two_activities/app/src/main/java/com/wordpress/jesphu/twoactivities/MainActivity.java
@@ -2,12 +2,19 @@ package com.wordpress.jesphu.twoactivities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launch_second_activity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
     }
 }
